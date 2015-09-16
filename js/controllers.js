5f60615d73d4d94b28b3e24e7c1216773b46db32
Mauro Bianchi <bianchimro@gmail.com>
2015-09-17 01:02:11 +0200

    many places for the map, and new theme

--- js/controllers.js
+++ js/controllers.js
@@ -87,6 +87,79 @@ themes.sunrise = {
     water  : { fill: "#000"}
 };
 
+themes.ruperstrian = {
+    background : { background : "#000"},
+    map : {
+        xgradient : {
+            color1 : "#FF512F",
+            color2 : "#F09819",
+            x1 : "10%",
+            x2 : "90%"
+        },
+
+        style  : {
+            stroke : "#fff",
+            "stroke-width" : 2
+        }
+    },
+
+    text : {
+        style  : {
+            stroke : "none",
+            "font-size": "45px",
+            "fill" : "#000",
+            "fill-opacity" : 0.7
+        },
+        title : {
+            style  : {
+               stroke : "#000",
+               fill : "#fff",
+               "font-family" : "Courier",
+               "font-weight" : "bold",
+
+            }    
+        }
+    },
+
+    major_road  : { stroke: "crimson",  "stroke-width": "3px", },
+    minor_road  : { stroke: "crimson",  "stroke-width": "2px" },
+    highway  : { stroke: "#fff",  "stroke-width": "3px" },
+    buildings  : { stroke: "white",  "stroke-width": "1px" },
+    //water  : { fill: "#000"}
+};
+
+
+
+
+var places = {
+    "Milano" : {
+        x : 9.1904984,
+        y : 45.4667971
+
+    },
+
+    "Paris" : {
+        x : 2.3521334,
+        y : 48.8565056
+    },
+
+    "New York" : {
+
+        x : -74.005941,
+        y : 40.712784
+    },
+
+    "London" : {
+        y: 51.5073219,
+        x: -0.1276474,
+    },
+
+    "Kathmandu" : {
+        y: "27.7076762",
+        x: "85.3148882"
+    }
+}
+
 
 angular.module('wallmaps')
 
@@ -98,15 +171,18 @@ angular.module('wallmaps')
         x : 9.669781,
         y : 45.698371
     };
-    */
+    
     $scope.center = {
         x : -74.005941,
         y : 40.712784, 
     };
+
+    */
     
-    
-    $scope.zoomfactor = 1;
+    $scope.places = places;
+    $scope.zoomfactor = 2;
     $scope.themeName = "sunrise";
+    $scope.placeName = "Milano"
 
     $scope.$watch('themeName', function(nv){
         if(!nv){
@@ -115,7 +191,15 @@ angular.module('wallmaps')
         $scope.theme = themes[nv];
     });
 
-    $scope.title= "New York City";
+    $scope.$watch('placeName', function(nv){
+        if(!nv){
+            return;
+        }
+        $scope.center = places[nv];
+        $scope.title = nv;
+    });
+
+    
 
     $scope.layout = {
         doc : {
@@ -128,7 +212,7 @@ angular.module('wallmaps')
         text : {
             margin : { top : 720, left: 20, right : 20, bottom: 20},
             title : {
-                x : 400,
+                x : 360,
                 y : 40
             },
             "title-style" : {
@@ -149,7 +233,7 @@ angular.module('wallmaps')
         text : {
             margin : { top : 720, left: 20, right : 20, bottom: 20},
             title : {
-                x : 400,
+                x : 380,
                 y : 40
             },
             "title-style" : {
